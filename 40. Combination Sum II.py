6bb34a512ab831498083ce428b396a5db4a5e40c
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2022-08-29 12:30:10 +0530

    40. Combination sum II fixed solution

--- 40. Combination Sum II.py
+++ 40. Combination Sum II.py
@@ -2,28 +2,27 @@ from typing import List
 
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = set()
-        tb = set() # target builder. 
+        res = []
+        tb = [] # target builder. 
         visited = [False]*len(candidates)
-        
+        candidates.sort()
         def dfs(t, i):
-            if t == 0 and tb not in res: 
-                res.add(tb.copy())
+            if t == 0:
+                res.append(tb.copy())
+            if t < 0: 
                 return 
-            
-            for j in range(len(candidates)):
-                if j == i or visited[j] == True:
+
+            prev = -1 
+            for j in range(i, len(candidates)):
+                if candidates[j] == prev:
                     continue
-                if candidates[j] <= t: 
-                    visited[j] = True 
-                    tb.add(candidates[j])
-                    dfs(t - candidates[j], j)
-                    # backtrack.
-                    visited[j] = False  
-                    tb.remove(candidates[j])
-        
+                tb.append(candidates[j])
+                dfs(t - candidates[j], j + 1)
+                tb.pop()
+                prev = candidates[j]
+
         dfs(target, 0)
-        
+        return res
         
 
 S = Solution() 
