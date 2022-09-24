8922e2e01ab3f20fa666483f8ded006d28fd2300
Apekshik Panigrahi <46071272+apekshik@users.noreply.github.com>
2022-09-24 09:37:59 -0500

    more problems solved

---
+++ 34. Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,14 @@
+"""
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+"""
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
